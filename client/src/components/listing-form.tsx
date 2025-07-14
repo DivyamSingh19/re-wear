82c54c1c238126da26decc244bd586355bc1969f
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-07-14 22:09:33 +0530

    stuff

--- client/src/components/listing-form.tsx
+++ client/src/components/listing-form.tsx
@@ -3,6 +3,13 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 //{className,...props} it means that i want all valid props that a regular <form> tag can take eg action,method etc.
 export function ListingForm({
@@ -12,7 +19,7 @@ export function ListingForm({
   return (
     <form
       className={cn(
-        "flex flex-col gap-5 bg-[#FFF9D9] rounded-3xl p-6",
+        "flex flex-col gap-5 bg-[#FFF9D9] rounded-3xl p-6 border border-black  shadow-black shadow-2xl",
         className
       )}
       {...props}
@@ -26,7 +33,13 @@ export function ListingForm({
 
       <div className="flex flex-col gap-3">
         <Label htmlFor="title">Title</Label>
-        <Input id="title" type="text" placeholder="Levi's Jeans" required />
+        <Input
+          id="title"
+          type="text"
+          placeholder="Levi's Jeans"
+          className="bg-[#dbc8b0] border border-black"
+          required
+        />
       </div>
 
       <div className="flex flex-col gap-3">
@@ -36,44 +49,100 @@ export function ListingForm({
           placeholder="Describe your item in detail..."
           required
           rows={4}
+          className="bg-[#dbc8b0] border border-black"
         />
       </div>
 
-      <div className="flex justify-start gap-4 md:gap-20">
+      <div className="flex justify-start gap-8 md:gap-20">
         <div className="flex flex-col gap-3 flex-1">
           <Label htmlFor="category">Category</Label>
-          <Input id="category" type="text" placeholder="Clothing" required />
+          <Input
+            id="category"
+            type="text"
+            placeholder="Clothing"
+            className="bg-[#dbc8b0] w-[180px] border border-black"
+            required
+          />
         </div>
         <div className="flex flex-col gap-3 flex-1">
           <Label htmlFor="type">Type</Label>
-          <Input id="type" type="text" placeholder="Jeans" required />
+          <Input
+            id="type"
+            type="text"
+            placeholder="Jeans"
+            className="bg-[#dbc8b0] w-[180px] border border-black"
+            required
+          />
         </div>
       </div>
 
-      <div className="flex justify-start gap-4 md:gap-20">
+      <div className="flex justify-start gap-8 md:gap-20">
         <div className="flex flex-col gap-3 flex-1">
           <Label htmlFor="size">Size</Label>
-          <Input id="size" type="text" placeholder="M" required />
+          <Select>
+            <SelectTrigger className="w-[180px] bg-[#dbc8b0] border border-black">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="small">S</SelectItem>
+              <SelectItem value="medium">M</SelectItem>
+              <SelectItem value="large">L</SelectItem>
+              <SelectItem value="extra-large">XL</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
         <div className="flex flex-col gap-3 flex-1">
           <Label htmlFor="tags">Tags</Label>
-          <Input id="tags" type="text" placeholder="vintage, casual" required />
+          <Select>
+            <SelectTrigger className="w-[180px] bg-[#dbc8b0] border border-black">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="yes">Yes</SelectItem>
+              <SelectItem value="no">No</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
       </div>
 
-      <div className="flex flex-col gap-3">
-        <Label htmlFor="condition">Condition</Label>
-        <Input
-          id="condition"
-          type="text"
-          placeholder="New, Like New, Good, Fair"
-          required
-        />
+      <div className="flex justify-start gap-8 md:gap-20">
+        <div className="flex flex-col gap-3 flex-1">
+          <Label htmlFor="condition">Condition</Label>
+          <Select>
+            <SelectTrigger className="w-[180px] bg-[#dbc8b0] border border-black">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="new">New</SelectItem>
+              <SelectItem value="like-new">Like-New</SelectItem>
+              <SelectItem value="fair">Fair</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-col gap-3 flex-1">
+          <Label htmlFor="gender">Gender</Label>
+          <Select>
+            <SelectTrigger className="w-[180px] bg-[#dbc8b0] border border-black">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="male">Male</SelectItem>
+              <SelectItem value="female">Female</SelectItem>
+              <SelectItem value="unisex">Unisex</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
 
       <div className="flex flex-col gap-3">
         <Label htmlFor="image">Image</Label>
-        <Input id="image" type="file" accept="image/*" required/>
+        <Input
+          id="image"
+          type="file"
+          accept="image/*"
+          className="bg-[#dbc8b0] border border-black"
+          required
+        />
       </div>
 
       <Button type="submit" className="w-full">
